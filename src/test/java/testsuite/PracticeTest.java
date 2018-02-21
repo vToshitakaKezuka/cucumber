b19fea680a66c18574bfd37057411bacaa4a85b6
毛塚 敏孝 <toshitaka.kezuka@Veriserve.co.jp>
2018-02-21 17:48:57 +0900

    testtest

---
+++ src/test/java/testsuite/PracticeTest.java
@@ -0,0 +1,17 @@
+package testsuite;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeTest {
+	private static WebDriver driver;
+	
+	@Test
+	public void test01(){
+		System.setProperty("webdriver.chrome.driver", "C:\\pleiades\\workspace\\webdriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.google.co.jp");
+	}
+
+}
